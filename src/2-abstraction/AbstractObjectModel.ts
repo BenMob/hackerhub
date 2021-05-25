211fb0f236a7f7e1077c663e03c8760e1af036de
Benjamin <omben001@cougars.csusm.edu>
2021-05-25 00:08:20 -0700

    Adds Abstraction Layer Package

---
+++ src/2-abstraction/AbstractObjectModel.ts
@@ -0,0 +1,31 @@
+/**
+ * This class is the Abstract class that all the Object Models will inherit from.
+ */
+abstract class AbstractObjectModel extends Object{
+    private id: number | undefined = undefined;
+
+    public AbstractObjectModel(){}
+
+    /**
+     * Sets the id of the object
+     * @param id 
+     */
+    public setId(id: number) : void{
+        this.id = id;
+    }
+
+    /**
+     * Gets the id of the Object
+     * @returns 
+     */
+    public getId(): number | undefined{
+        return this.id;
+    }
+
+    /**
+     * Returns trues this specific Objec has a builder Object 
+     */
+    public abstract hadBuilder(): boolean;
+}
+
+export default AbstractObjectModel;
