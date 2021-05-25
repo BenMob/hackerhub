73a16ab20b66d726e63b77b4d06134401464e752
Benjamin <omben001@cougars.csusm.edu>
2021-05-25 10:37:43 -0700

    Add Object Model Interface

---
+++ src/2-abstraction/IObjectModel.ts
@@ -0,0 +1,12 @@
+/**
+ * The Object Model Interface on the Abstraction Level
+ */
+
+interface IObjectModel{
+    /**
+     * Determines wehther the following Object has a 
+     */
+    hasBuilder(): boolean
+}
+
+export default IObjectModel
