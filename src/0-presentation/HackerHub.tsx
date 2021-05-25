ad8194db03b91a6c1c61d17724d8871477c8cd88
Benjamin <omben001@cougars.csusm.edu>
2021-05-25 00:07:26 -0700

    Adds presentation layer package

---
+++ src/0-presentation/HackerHub.tsx
@@ -0,0 +1,11 @@
+/**
+ * This is the root of the HackerHub App Component
+ * @returns 
+ */
+function HackerHub(): JSX.Element{
+    return(
+        <h1>Welcome To HackerHub</h1>
+    )
+}
+
+export default HackerHub
