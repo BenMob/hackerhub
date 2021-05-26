7b1d3b3f166c9db7494c5c1870755aad4b28f41a
Benjamin <omben001@cougars.csusm.edu>
2021-05-25 17:15:20 -0700

    Rename apis package to api

---
+++ src/1-api/GithubAPI.ts
@@ -0,0 +1,16 @@
+import AbstractAPI from "../2-abstraction/AbstractAPI";
+import AbstractObjectModel from "../3-service/objectmodeling/AbstractObjectModel";
+
+class GitHubAPI extends AbstractAPI{
+    protected post(endpoint: String | undefined, params: Object): String {
+        throw new Error("Method not implemented.");
+    }
+    protected parseResponse(dataString: String): Object {
+        throw new Error("Method not implemented.");
+    }
+    protected buildObjectModel(object: Object): AbstractObjectModel {
+        throw new Error("Method not implemented.");
+    }
+}
+
+export default GitHubAPI;
