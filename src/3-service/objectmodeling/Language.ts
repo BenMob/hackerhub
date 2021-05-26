f1f4abfcd570ceb8870f9c1effcfba205009d521
Benjamin <omben001@cougars.csusm.edu>
2021-05-25 21:59:47 -0700

    Adds Language class

---
+++ src/3-service/objectmodeling/Language.ts
@@ -0,0 +1,67 @@
+/**
+ * The Language Object
+ */
+
+import AbstractObjectModel from "./AbstractObjectModel";
+
+class Language extends AbstractObjectModel{
+
+    /*
+     * Attributes 
+     */
+    private name: String | undefined
+    private lineCount: number | undefined
+    private color: String | undefined
+
+    Language(name: String){
+        this.name = name;
+    }
+
+    /**
+     * Sets the line count 
+     * @param count 
+     */
+    public setLineCount(count: number): void {
+        this.lineCount = count;
+    }
+
+    /**
+     * Sets the color used to represent this language on the User Interface
+     * @param color 
+     */
+    public setColor(color: String): void {
+        this.color = color;
+    }
+
+    /**
+     * 
+     * @returns Gets the name of the language
+     */
+    public getName(): String | undefined{
+        return this.name;
+    }
+
+    /**
+     * Returns the number of lines occuppied by the language
+     */
+    public getLineCount(): number | undefined{
+        return this.lineCount;
+    }
+
+    /**
+     * Returns the color of that represents this language 
+     * @returns color
+     */
+    public getColor(): String | undefined{
+        return this.color;
+    }
+
+    /**
+     * Returns true if this object has a builder 
+     */
+    public hasBuilder(): boolean {
+        return true;
+    }
+}
+
+export default Language;
