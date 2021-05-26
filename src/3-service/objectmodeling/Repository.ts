b5487ebcadb34e6e256fe4463e8b8f5e2b3623fc
Benjamin <omben001@cougars.csusm.edu>
2021-05-25 22:39:24 -0700

    Adds the Repository Class

---
+++ src/3-service/objectmodeling/Repository.ts
@@ -0,0 +1,72 @@
+/**
+ * This is the repository class
+ */
+
+import AbstractObjectModel from "./AbstractObjectModel";
+
+class Repository extends AbstractObjectModel{
+    private owner: String | undefined
+    private name: String | undefined
+    private description: String | undefined
+    private url: String | undefined
+    private websiteURL: String | undefined
+
+    /*
+     * Setters 
+     */
+    public setOwner(owner: String): Repository{
+        this.owner = owner;
+        return this;
+    }
+
+    public setName(name : String): Repository{
+        this.name = name;
+        return this;
+    }
+
+    public setDescription(description: String): Repository{
+        this.description = description;
+        return this;
+    }
+
+    public setURL(url: String): Repository{
+        this.url = url;
+        return this;
+    }
+
+    public setWebSiteURL(url: String): Repository{
+        this.websiteURL = url;
+        return this;
+    }
+
+    /*
+     * Getters  
+     */
+    public getOwner(): String | undefined{
+        return this.owner;
+    }
+
+    public getName(): String | undefined{
+        return this.name;
+    }
+
+    public getDescription(): String | undefined{
+        return this.description;
+    }
+
+    public getUrl(): String | undefined{
+        return this.url;
+    }
+
+    public getWebsiteUrl(): String | undefined{
+        return this.websiteURL
+    }
+
+
+    public hasBuilder(): boolean {
+        throw new Error("Method not implemented.");
+    }
+    
+}
+
+export default Repository
