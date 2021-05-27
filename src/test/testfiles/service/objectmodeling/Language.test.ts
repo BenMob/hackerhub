239b5a9d2b49e64e33f94f08bd4edc4b6c595d9d
Benjamin <omben001@cougars.csusm.edu>
2021-05-27 11:48:13 -0700

    Adds test cases for Account.ts

--- src/test/testfiles/service/objectmodeling/Language.test.ts
+++ src/test/testfiles/service/objectmodeling/Language.test.ts
@@ -1,7 +1,54 @@
-describe("Sample Test", () => {
-    test("Ready to Test", () => {
-        expect("ready").toBe("ready")
+/**
+ * Tests the Language.ts class
+ */
+
+import Language from "../../../../service/objectmodeling/Language";
+import testData from "../../../testdata/service/objectmodeling/language.json"
+
+/**
+ * Object to be tested
+ */
+let language: Language | undefined;
+
+/**
+ * Runs before each test
+ */
+beforeEach(() => {
+    language = new Language(testData.name);
+})
+
+/**
+ * Runs after all test
+ */
+afterAll(() => {
+    language = undefined;
+})
+
+describe("Language.ts Test", () => {
+    test("Testing constructor(name: String)", () => {
+        expect(language?.getName()).toBe(testData.name);
+        expect(language?.getId()).toBeDefined();
+        expect(language?.hasId()).toBeTruthy();
     });
+
+    test("Testing hasBuilder()", () => {
+        expect(language?.hasBuilder()).toBeFalsy();
+    })
+
+    test("Testing setId() and getId()", () =>{
+        language?.setId(testData.id);
+        expect(language?.getId()).toBe(testData.id);
+    })
+
+    test("Testing setLineCount() and getLineCount()", () => {
+        language?.setLineCount(testData.lineCount);
+        expect(language?.getLineCount()).toBe(testData.lineCount);
+    })
+
+    test("Testing setColor() and getColor()", () => {
+        language?.setColor(testData.color);
+        expect(language?.getColor()).toBe(testData.color);
+    })
 });
 
 export{}
