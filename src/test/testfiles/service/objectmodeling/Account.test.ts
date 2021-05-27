fc15c29f5680d0bde3ecc0145d5bb6865d87dace
Benjamin <omben001@cougars.csusm.edu>
2021-05-27 06:32:07 -0700

    Adds Account test file

--- src/test/testfiles/service/objectmodeling/Account.test.ts
+++ src/test/testfiles/service/objectmodeling/Account.test.ts
@@ -1,7 +1,57 @@
-describe("Sample Test", () => {
-    test("Ready to Test", () => {
-        expect("ready").toBe("ready")
+/**
+ * Tests the Account.ts class
+ */
+
+import Account from "../../../../service/objectmodeling/Account";
+import testData from "../../../testdata/service/objectmodeling/account.json"
+
+/**
+ * Object to be tested
+ */
+let account: Account | undefined;
+
+/**
+ * Runs before each test 
+ */
+beforeEach(() => {
+    account = new Account();
+})
+/** 
+ * Runs after all the tests
+ */
+afterAll(() => {
+    account = undefined;
+})
+
+/**
+ * Account.ts Test suit
+ */
+describe("Account.ts Test", () => {
+    test("Testing getUsername() and setUsername()", () => {
+        account?.setUsername(testData.username);
+        expect(account?.getUsername()).toBe(testData.username);
     });
+
+    test("Testing getUserFullName() and setUserFullName()", () => {
+        account?.setUserFullName(testData.userFullName);
+        expect(account?.getUserFullName()).toBe(testData.userFullName);
+    });
+
+    test("Testing getAvatarURL() and setAvatarURL()", () => {
+        account?.setAvatarURL(testData.avatarUrl);
+        expect(account?.getAvatarURL()).toBe(testData.avatarUrl);
+    })
+
+    test("Testing getCompany() and setCompany()", () => {
+        account?.setCompany(testData.company);
+        expect(account?.getCompany()).toBe(testData.company);
+    })
+
+    test("Testing getBlogURL() and setBlogURL()", () => {
+        account?.setBlogURL(testData.blogURL);
+        expect(account?.getBlogURL()).toBe(testData.blogURL);
+    })
+
 });
 
 export{}
